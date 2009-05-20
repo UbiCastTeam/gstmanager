055af40f9aaa02cecb9e105ce0968da6f58a9bdd
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-05-20 15:13:54 +0000

    Added installer and readme

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, imp
+from distutils.core import setup
+
+#gstmanager = imp.load_source("version", "version.py")
+VERSION = "0.1"
+
+setup(
+    name="gstmanager",
+    #version=gstmanager.VERSION,
+    version=VERSION,
+    description="gstmanager is a helper for building gstreamer applications",
+    author="Florent Thiery",
+    author_email="florent.thiery@ubicast.eu",
+    url="http://code.google.com/p/gstmanager/",
+    license="GNU/LGPLv3",
+    packages = ['gstmanager'],
+)
