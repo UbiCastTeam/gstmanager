dd89adcdd078f30e7503e82126198bea16a9018d
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-05-27 17:10:20 +0000

    Added other bins

--- gstmanager/bins/previewtee.py
+++ gstmanager/bins/previewtee.py
@@ -1,18 +1,19 @@
 import gst
-from binmanager import BinManager
+from gstmanager.binmanager import BinManager
 
 class PreviewTee(BinManager):
     # This Bin is intended to serve as plug-and-preview element
 
     def __init__(self):
-        BinManager.__init__(self)
+        BinManager.__init__(self, name="PreviewTee")
         q0 = gst.element_factory_make("queue", "q0")
         tee = gst.element_factory_make("tee", "decoded_tee")
         q1 = gst.element_factory_make("queue", "q1")
         q2 = gst.element_factory_make("queue", "q2")
         ff = gst.element_factory_make("ffmpegcolorspace")
-        #vsink = gst.element_factory_make("xvimagesink")
-        vsink = gst.element_factory_make("glimagesink")
+        vsink = gst.element_factory_make("xvimagesink")
+        #vsink = gst.element_factory_make("glimagesink")
+        #vsink.set_property("sync", False)
 
         self.add(q0, tee, q1, q2, ff, vsink)
 
@@ -23,8 +24,8 @@ class PreviewTee(BinManager):
         tee.link(q2)
 
         output_pad = q2.get_pad("src")
-        self.add_ghostpad_from_static("output", output_pad)
+        self.add_ghostpad_from_static("voutput", output_pad)
 
         input_pad = q0.get_pad("sink")
-        self.add_ghostpad_from_static("input", input_pad)
+        self.add_ghostpad_from_static("vinput", input_pad)
 
--- gstmanager/bins/rtpencodingbin.py
+++ gstmanager/bins/rtpencodingbin.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+#os.environ['GST_DEBUG'] = '3'
+#os.environ['GST_DEBUG'] = '2'
+
 import logging
 import gst
 from gstmanager.binmanager import BinManager
@@ -21,13 +25,13 @@ class RtpEncodingBin(BinManager):
 
         self.vpay = vpay = self.add_element("rtph264pay")
         # Note: this does not work on Ubuntu 8.04
-        vpad = vpay.get_pad("src")
-        vpad.connect('notify::caps', self.notify_caps)
+        #vpad = vpay.get_pad("src")
+        #vpad.connect('notify::caps', self.notify_caps)
 
         self.vsink = vsink = self.add_element("udpsink")
-        self.vsink.set_properties("host", profile.ip, "port", profile.vport)
+        vsink.set_property("host", profile.ip)
+        vsink.set_property("port", profile.vport)
         self.set_caps(vcaps, "video/x-raw-yuv, format=(fourcc)I420, framerate=(fraction)%s/1, width=(int)%s, height=(int)%s, pixel-aspect-ratio=(fraction)1/1" %(profile.framerate, profile.width, profile.height))
-
 
         vqueue.link(vproc)
         vproc.link(vcaps)
@@ -39,36 +43,46 @@ class RtpEncodingBin(BinManager):
         self.add_ghostpad_from_static("vinput", vinput_pad)
 
         self.aqueue = aqueue = self.add_element("queue")
+        self.aconv = aconv = self.add_element("audioconvert")
         self.aenc = aenc = self.add_element("faac")
         aenc.set_property("bitrate", profile.abitrate)
         self.apay = apay = self.add_element("rtpmp4gpay")
-        self.vsink = vsink = self.add_element("udpsink")
-        self.vsink.set_properties("host", profile.ip, "port", profile.aport)
+        self.asink = asink = self.add_element("udpsink")
+        self.asink.set_property("host", profile.ip)
+        asink.set_property("port", profile.aport)
 
-        aqueue.link(aenc)
+        aqueue.link(aconv)
+        aconv.link(aenc)
         aenc.link(apay)
-        apay.link(vsink)
+        apay.link(asink)
 
         ainput_pad = aqueue.get_pad("sink")
         self.add_ghostpad_from_static("ainput", ainput_pad)
+
+        print vsink.get_property("port")
 
     def notify_caps(self, pad, caps):
         caps =  pad.get_caps()
         print caps
 
     def get_sdp_info(self):
-        #print self.get_state()
-        #if self.get_state() == GST_STATE_PLAYING:
-        if True:
-            paypad = self.vsink.get_pad("sink")
-            #caps = paypad.get_caps()
-            caps = paypad.get_negotiated_caps()
-            caps = caps.to_string()
-            logger.debug("Negociated caps are %s" %caps)
-            return False
-        else:
-            logger.debug("Need to run the Bin first to get negociated caps")
-            return False
+        sdp_template = \
+"v=0\n\
+o=- 571622436 4192730712 IN IP4 127.0.0.1\n\
+s=Gstreamer test\n\
+i=Gstreamer-fastvdo test\n\
+u=http://www.ubicast.eu\n\
+e=support@fastvdo.com\n\
+c=IN IP4 %s/64\n\
+m=video %s RTP/AVP 96\n\
+a=rtpmap:96 H264/90000\n\
+a=control:trackID=1\n\
+a=fmtp:96 profile-level-id=42e01e; packetization-mode=1; sprop-parameter-sets=Z0LgHtoC0EmwEAg=,aM4zyA==\n\
+m=audio %s RTP/AVP 96\n\
+a=rtpmap:96 mpeg4-generic/48000/2\n\
+a=control:trackID=2\n\
+a=fmtp:96 streamtype=5; profile-level-id=1; mode=AAC-hbr; config=0990; objectType=64; sizeLength=13; indexLength=3; indexDeltaLength=3" %(self.profile.ip, self.profile.vport, self.profile.aport)
+        print sdp_template
 
 if __name__ == '__main__':
 
@@ -81,17 +95,34 @@ if __name__ == '__main__':
     )
 
     from gstmanager.profile import DefaultStreamingProfile
-    profile = DefaultStreamingProfile() 
+    profile = DefaultStreamingProfile()
+    profile.to_string()
 
-    test = RtpEncodingBin(profile)
+    rtpsink = RtpEncodingBin(profile)
 
-    p = gst.parse_launch("videotestsrc ! queue name=src")
-    src = p.get_by_name("src")
-    p.add(test)
-    src.link(test)
+    from gstmanager.bins.testsrcbin import TestSrcBin
+    src = TestSrcBin() 
+
+    from gstmanager.bins.previewtee import PreviewTee
+    preview = PreviewTee()
+
+    p = gst.Pipeline()
+
+    p.add(src, preview, rtpsink)
+    voutput = src.get_pad("voutput")
+    vinput = preview.get_pad("vinput")
+    voutput.link(vinput)
+    voutput2 = preview.get_pad("voutput")
+    vinput2 = rtpsink.get_pad("vinput")
+    voutput2.link(vinput2)
+    aoutput = src.get_pad("aoutput")
+    ainput = rtpsink.get_pad("ainput")
+    aoutput.link(ainput)
 
     p.set_state(gst.STATE_PLAYING)
 
     import gtk
     gtk.gdk.threads_init()
+    #import gobject
+    #gobject.timeout_add(1000, test.get_sdp_info)
     gtk.main()
