d4bbdd8271fd3a39dd23e71d020bc22f646b224a
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-05-27 19:06:47 +0000

    Corrected module renaming

--- gstmanager/bins/rtpencoding.py
+++ gstmanager/bins/rtpencoding.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 
     rtpsink = RtpEncodingBin(profile)
 
-    from gstmanager.bins.testsrcbin import TestSrcBin
+    from gstmanager.bins.testsrc import TestSrcBin
     src = TestSrcBin() 
 
     from gstmanager.bins.previewtee import PreviewTee
