c7ebcebfb083f16e5d04fc5be03b0bfe89dcbcca
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-07-27 17:51:41 +0000

    Ported test for sbinmanager

--- tests/test_sbinmanager.py
+++ tests/test_sbinmanager.py
@@ -3,7 +3,13 @@
 
 # SBins are for "String Bins": manipulating Bins directly with Python can be tricky, notably because of the API differences (and lack of documentation porting) with the C API. Using string-based bin-like manipulation offers some flexibility over raw bin programming
 
-# Note: SBinManager is not released yet. 
+import logging, sys
+
+logging.basicConfig(
+    level=getattr(logging, "DEBUG"),
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    stream=sys.stderr
+)
 
 from gstmanager.sbins.sources.videotest import VideoTestSource
 v = VideoTestSource()
@@ -11,20 +17,42 @@ v = VideoTestSource()
 from gstmanager.sbins.sinks.ximagesink import XImageSink
 s = XImageSink()
 
-pipeline_desc = "%s ! %s" %(v.sbin, s.sbin)
-print pipeline_desc
+from gstmanager.sbins.analysis.brightness import BrightnessVideoAnalyse
+a = BrightnessVideoAnalyse()
+
+from gstmanager.sbins.sources.audiotest import AudioTestSource
+s2 = AudioTestSource()
+
+from gstmanager.sbins.analysis.level import LevelAnalyser
+a2 = LevelAnalyser()
+
+from gstmanager.sbins.sinks.alsa import AlsaSink
+s3 = AlsaSink()
+
+from gstmanager.sbins.encoders.ogg import OggEncoder
+e = OggEncoder("/tmp/test.ogg")
+
+from gstmanager.sbinmanager import SBinManager
+man = SBinManager()
+
+'''
+man.add(v)
+man.add(s)
+man.add(a)
+man.add(s2)
+man.add(a2)
+man.add(s3)
+man.add(e)
+'''
+man.add_many(v, s, a, s2, a2, s3, e)
 
 if __name__ == '__main__':
-    import logging, sys
-
-    logging.basicConfig(
-        level=getattr(logging, "DEBUG"),
-        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        stream=sys.stderr
-    )
 
     from gstmanager.gstmanager import PipelineManager
-    pipelinel = PipelineManager(pipeline_desc)
+    pipelinel = PipelineManager(man.pipeline_desc)
     pipelinel.run()
     import gtk
+    import gobject
+    gobject.timeout_add(2000, pipelinel.send_eos)
+    gobject.timeout_add(3500, man.get_pipeline)
     gtk.main()
