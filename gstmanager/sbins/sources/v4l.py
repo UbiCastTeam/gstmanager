c57530845d4b1b3ed6537f0583cc538d018fc090
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-08-13 16:48:04 +0000

    Added optional argument to specify V4L version

--- gstmanager/sbins/sources/v4l.py
+++ gstmanager/sbins/sources/v4l.py
@@ -5,8 +5,8 @@ from gstmanager.sbins.source import VideoSource
 
 class V4LSource(VideoSource):
     # V4L Source class
-    def __init__(self, device_id="/dev/video0"):
+    def __init__(self, device_id="/dev/video0", v4l_version=None):
         self.description = "V4L source"
         self.type = "video"
-        sbin = "v4lsrc device=%s" %device_id
+        sbin = "v4l%ssrc device=%s" %(v4l_version, device_id)
         VideoSource.__init__(self, sbin)
