6f63a4b38957a00f6cdd1dc46d7c653fa508d913
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-08-13 17:49:21 +0000

    Renamed default ogg profile

--- gstmanager/profiles/ogg.py
+++ gstmanager/profiles/ogg.py
@@ -5,7 +5,7 @@ class OggRecordingProfile(DefaultEncodingProfile):
     def __init__(self):
         DefaultEncodingProfile.__init__(self)
         self.extension = "ogg"
-        self.video_width = 512
-        self.video_height = 384
-        self.video_bitrate = 3000
+        self.video_width = 320
+        self.video_height = 240
+        self.video_bitrate = 2000
         self.audio_bitrate = 128000
