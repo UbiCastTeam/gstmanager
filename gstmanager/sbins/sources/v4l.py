138cffed8e905895ae21f604ebb145aad6802168
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-09-18 19:06:30 +0000

    Now will use v4l1 if no v4l version argument is given

--- gstmanager/sbins/sources/v4l.py
+++ gstmanager/sbins/sources/v4l.py
@@ -5,7 +5,7 @@ from gstmanager.sbins.source import VideoSource
 
 class V4LSource(VideoSource):
     # V4L Source class
-    def __init__(self, device_id="/dev/video0", v4l_version=None):
+    def __init__(self, device_id="/dev/video0", v4l_version=""):
         self.description = "V4L source"
         if v4l_version == "1":
             v4l_version = ""
