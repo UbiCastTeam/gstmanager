71e8e660af1c64f2faf50c1b33173ebfdd59e4e1
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2009-09-18 19:52:49 +0000

    queues are mostly useless before tees

--- gstmanager/sbins/source.py
+++ gstmanager/sbins/source.py
@@ -10,9 +10,9 @@ class AVSource(object):
 class AudioSource(object):
     def __init__(self, sbin_content):
         self.tags = ["a_src"]
-        self.sbin = "%s ! queue ! tee name=%s_tee" %(sbin_content, self.tags[0])
+        self.sbin = "%s ! tee name=%s_tee" %(sbin_content, self.tags[0])
 
 class VideoSource(object):
     def __init__(self, sbin_content):
         self.tags = ["v_src"]
-        self.sbin = "%s ! queue ! tee name=%s_tee" %(sbin_content, self.tags[0])
+        self.sbin = "%s ! tee name=%s_tee" %(sbin_content, self.tags[0])
