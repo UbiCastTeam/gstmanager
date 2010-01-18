70b3e237a2a24b206e572a2212c8f8fb19aa9f54
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2010-01-18 17:36:08 +0000

    allow id passing

--- gstmanager/sbins/sinks/alsa.py
+++ gstmanager/sbins/sinks/alsa.py
@@ -4,8 +4,8 @@
 from gstmanager.sbins.sink import AudioSink
 
 class AlsaSink(AudioSink):
-    def __init__(self, sync=True): 
+    def __init__(self, sync=True, device_id=0): 
         self.description = "Alsa audio sink"
         self.type = "audio"
-        sbin = "alsasink sync=%s" %sync
+        sbin = "alsasink sync=%s device=hw:%s" %(sync, device_id)
         AudioSink.__init__(self, sbin)
