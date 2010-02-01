79a62f57aa7d9ce016fb4de2bdf149f20ab22635
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2010-02-01 21:49:44 +0000

    added udp stats example

--- tests/test_caps_reporting.py
+++ tests/test_caps_reporting.py
@@ -1,43 +1,41 @@
-from gstmanager.event import EventListener 
+import easyevent
+import gobject
+import time
 
-import logging
-logger = logging.getLogger("test_caps_reporting")
-
-class Actioner(EventListener):
-    # This class will subscribe to proxied eos messages
+class Actioner(easyevent.User):
     def __init__(self):
-        EventListener.__init__(self)
-        self.registerEvent("eos")
-        self.registerEvent("caps")
-
-    def evt_eos(self, event):
-    # This is the callback used for every evt_MSGNAME received
-        logger.info("EOS Recieved")
+        easyevent.User.__init__(self)
+        self.register_event("caps")
 
     def evt_caps(self, event):
-        logger.info("Caps received, %s" %event.content)
+        print "Caps received, %s" %event.content
 
 if __name__ == '__main__':
-    import logging, sys
-
-    logging.basicConfig(
-        level=getattr(logging, "DEBUG"),
-        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        stream=sys.stderr
-    )
 
     a = Actioner()
 
     from gstmanager.gstmanager import PipelineManager
-    pipeline_desc = "audiotestsrc ! faac ! rtpmp4gpay name=pay ! udpsink"
+    pipeline_desc = "audiotestsrc ! faac ! rtpmp4gpay name=pay ! udpsink host=127.0.0.1 port=1234 name=sink"
 
     pipelinel = PipelineManager(pipeline_desc)
 
-    import gobject
+    def get_time(time_epoch):
+        if time_epoch == 0:
+            time_epoch = "never"
+        else:
+            time_epoch = time.ctime(time_epoch/1000000000)
+        return time_epoch
+
+    def get_udp_stats():
+        sink = pipelinel.pipeline.get_by_name("sink")
+        bytes_sent, packets_sent, connect_time, disconnect_time = sink.emit("get-stats", "127.0.0.1", 1234)
+
+        print "bytes sent: %s, packets sent: %s, connect_time: %s disconnect_time: %s" %(bytes_sent, packets_sent, get_time(connect_time), get_time(disconnect_time))
+        return True
+
+    gobject.timeout_add(1000, get_udp_stats)
     gobject.threads_init()
     gobject.idle_add(pipelinel.activate_caps_reporting_on_element, "pay")
     gobject.idle_add(pipelinel.run)
     import gtk
     gtk.main()
-
-
