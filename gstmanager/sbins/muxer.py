b5deb3883bf9a4ade7379eef754203ca698b73bb
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2012-10-05 10:13:45 +0000

    name queues for polling purposes

--- gstmanager/sbins/muxer.py
+++ gstmanager/sbins/muxer.py
@@ -5,5 +5,5 @@ class Muxer(object):
     index = 0
     def __init__(self, sbin_content):
         self.tags = ["a_enc_%s" %Muxer.index, "v_enc_%s" %Muxer.index]
-        self.sbin = "%s_tee. ! queue ! %s name=muxer ! queue ! tee name=muxer_tee %s_tee. ! queue ! muxer." %(self.tags[0], sbin_content, self.tags[1])
+        self.sbin = "%s_tee. ! queue name=a_mux_%s ! %s name=muxer ! queue ! tee name=muxer_tee %s_tee. ! queue name=v_mux_%s ! muxer." %(self.tags[0], Muxer.index, sbin_content, self.tags[1], Muxer.index)
         Muxer.index += 1
