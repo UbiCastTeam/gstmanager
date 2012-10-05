e7da5d70a98044b1af0e8bee918a457b395a4fb4
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2012-10-05 15:21:06 +0000

    increase audio queue size for muxers

--- gstmanager/sbins/muxer.py
+++ gstmanager/sbins/muxer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import gst
+audio_queue = 'queue max-size-bytes=0 max-size-buffers=0 max-size-time=%s' %(30*gst.SECOND)
+
 class Muxer(object):
     index = 0
     def __init__(self, sbin_content):
         self.tags = ["a_enc_%s" %Muxer.index, "v_enc_%s" %Muxer.index]
-        self.sbin = "%s_tee. ! queue name=a_mux_%s ! %s name=muxer ! queue ! tee name=muxer_tee %s_tee. ! queue name=v_mux_%s ! muxer." %(self.tags[0], Muxer.index, sbin_content, self.tags[1], Muxer.index)
+        self.sbin = "%s_tee. ! %s name=a_mux_%s ! %s name=muxer ! queue ! tee name=muxer_tee %s_tee. ! queue name=v_mux_%s ! muxer." %(self.tags[0], audio_queue, Muxer.index, sbin_content, self.tags[1], Muxer.index)
         Muxer.index += 1
