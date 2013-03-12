29e4b62a17273d2dc4abeb489e2ce71c2ed0f13a
fthiery <fthiery@9120d680-3a1e-11de-b5fc-91815ed80701>
2013-03-12 10:38:42 +0000

    remove useless queue after muxer

--- gstmanager/sbins/muxer.py
+++ gstmanager/sbins/muxer.py
@@ -8,5 +8,5 @@ class Muxer(object):
     index = 0
     def __init__(self, sbin_content):
         self.tags = ["a_enc_%s" %Muxer.index, "v_enc_%s" %Muxer.index]
-        self.sbin = "%s_tee. ! %s name=a_mux_%s ! %s name=muxer ! queue ! tee name=muxer_tee %s_tee. ! queue name=v_mux_%s ! muxer." %(self.tags[0], audio_queue, Muxer.index, sbin_content, self.tags[1], Muxer.index)
+        self.sbin = "%s_tee. ! %s name=a_mux_%s ! %s name=muxer ! tee name=muxer_tee %s_tee. ! queue name=v_mux_%s ! muxer." %(self.tags[0], audio_queue, Muxer.index, sbin_content, self.tags[1], Muxer.index)
         Muxer.index += 1
