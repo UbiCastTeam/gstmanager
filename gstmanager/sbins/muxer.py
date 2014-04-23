9e28c194db9361a80272690f9c2c85f84e122f1b
florent.thiery@ubicast.eu <florent.thiery@ubicast.eu@9120d680-3a1e-11de-b5fc-91815ed80701>
2014-04-23 17:30:30 +0000

    increase muxer queue size because it tends to fill with some slow starting sources

--- gstmanager/sbins/muxer.py
+++ gstmanager/sbins/muxer.py
@@ -3,10 +3,11 @@
 
 import gst
 audio_queue = 'queue max-size-bytes=0 max-size-buffers=0 max-size-time=%s' %(30*gst.SECOND)
+muxer_queue = 'queue max-size-bytes=0 max-size-buffers=0 max-size-time=%s' %(5*gst.SECOND)
 
 class Muxer(object):
     index = 0
     def __init__(self, sbin_content):
         self.tags = ["a_enc_%s" %Muxer.index, "v_enc_%s" %Muxer.index]
-        self.sbin = "%s_tee. ! %s name=a_mux_%s ! %s name=muxer ! tee name=muxer_tee %s_tee. ! queue name=v_mux_%s ! muxer." %(self.tags[0], audio_queue, Muxer.index, sbin_content, self.tags[1], Muxer.index)
+        self.sbin = "%s_tee. ! %s name=a_mux_%s ! %s name=muxer ! tee name=muxer_tee %s_tee. ! %s name=v_mux_%s ! muxer." %(self.tags[0], audio_queue, Muxer.index, sbin_content, self.tags[1], muxer_queue, Muxer.index)
         Muxer.index += 1
