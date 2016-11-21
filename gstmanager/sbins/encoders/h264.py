882f8d88bfa57f3217f84ed446af17e2afab19db
Anthony Violo <anthony.violo@ubicast.eu>
2016-11-21 10:34:25 +0100

    Update profile attribut to use with old and new type

--- gstmanager/sbins/encoders/h264.py
+++ gstmanager/sbins/encoders/h264.py
@@ -3,23 +3,32 @@
 
 from gstmanager.sbins.encoder import VideoEncoder
 from gstmanager.sbins.encoder import DefaultEncodingProfile
-
-class H264Encoder(VideoEncoder):
-    def __init__(self, bytestream="False", profile=DefaultEncodingProfile()):
-        self.description = "h264 encoder"
-        self.type = "video"
-        if hasattr(profile, 'keyframe_freq'):
-            keyframe_freq = profile.keyframe_freq
-        else:
-            keyframe_freq = 0
-        sbin = "videobalance name=vlivemute ! x264enc bitrate=%s threads=%s byte-stream=%s key-int-max=%s" %(profile.video_bitrate, profile.encoding_threads, bytestream, keyframe_freq)
-        VideoEncoder.__init__(self, sbin, profile)
-
 from gstmanager.sbins.encoder import AudioEncoder
 
+
+class H264Encoder(VideoEncoder):
+    def __init__(self, bytestream="False", profiles=DefaultEncodingProfile()):
+        self.description = "h264 encoder"
+        self.type = "video"
+        if isinstance(profiles, list):
+            main_profile = profiles[0]
+        else:
+            main_profile = profiles
+        if hasattr(main_profile, 'keyframe_freq'):
+            keyframe_freq = main_profile.keyframe_freq
+        else:
+            keyframe_freq = 0
+        sbin = "videobalance name=vlivemute ! x264enc bitrate=%s threads=%s byte-stream=%s key-int-max=%s" % (main_profile.video_bitrate, main_profile.encoding_threads, bytestream, keyframe_freq)
+        VideoEncoder.__init__(self, sbin, main_profile)
+
+
 class AACEncoder(AudioEncoder):
-    def __init__(self, profile=DefaultEncodingProfile()):
+    def __init__(self, profiles=DefaultEncodingProfile(), index=0):
         self.description = "AAC encoder"
         self.type = "audio"
-        sbin = "volume name=alivemute ! faac bitrate=%s profile=2" %profile.audio_bitrate
+        if isinstance(profiles, list):
+            main_profile = profiles[0]
+        else:
+            main_profile = profiles
+        sbin = "volume name=alivemute%s ! faac bitrate=%s profile=2" % (index, main_profile.audio_bitrate)
         AudioEncoder.__init__(self, sbin)
